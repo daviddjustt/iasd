5f59482d873cc39a58a90f90eddc0d470ce9ff22
daviddjustt <daviddjustt@gmail.com>
2026-01-09 08:43:07 -0300

    Initial commit

---
+++ iasd/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from iasd.users.models import User
+from iasd.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
